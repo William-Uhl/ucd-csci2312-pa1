74b0e37bc202a89bb0d2b91c2d425439329fa9bd
William-Uhl <William.uhl@ucdenver.edu>
2015-09-07 21:40:48 -0600

    Update Point.h

--- Point.h
+++ Point.h
@@ -1,4 +1,4 @@
-// A 2-dimensional point class!
+// A 3-dimensional point class!
 // Coordinates are double-precision floating point.
 
 #ifndef __point_h
@@ -9,11 +9,12 @@ class Point {
 private:
   double x;
   double y;
+    double z;
 
 public:
   // Constructors
   Point();                      // default constructor
-  Point(double x, double y);    // two-argument constructor
+  Point(double x, double y, double z);    // three-argument constructor
 
   // Destructor
   ~Point();
@@ -21,10 +22,16 @@ public:
   // Mutator methods
   void setX(double newX);
   void setY(double newY);
+    void setZ(double newZ);
 
   // Accessor methods
   double getX();
   double getY();
+    double getZ();
+
+    // distanceTo function, finds the distance between two points
+    double distanceTo(Point&);
+
 };
 
-#endif // __point_h
+#endif // __point_
